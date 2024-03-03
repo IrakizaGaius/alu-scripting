50ed1233440c35c5a0c027b72ef58be3e7c39f61
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2024-03-03 11:54:26 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """function that fetches number_of_subscribers"""
     URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    HEADERS = {"User-Agent": "Chrome/122.0.6261.95"}
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
 
     try:
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
