a61fdb8c2e77a014c6d4a4a077b0f4f1e617a23e
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2024-03-03 10:03:10 +0200

    Trial

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """function that fetches number_of_subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {"User-Agent ": "curl/7.64.1"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
