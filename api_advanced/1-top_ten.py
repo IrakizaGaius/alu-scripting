32d9ba1bb6901db4d4ee74378901f1fcf500203f
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2024-03-03 12:13:19 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
 
     sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
                             .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
+                            headers={"User-Agent": "Roland-23245"},
                             allow_redirects=False)
     if sub_info.status_code >= 300:
         print('None')
