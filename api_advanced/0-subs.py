51d88f50f1d7463e7c71468dcfcac9c3819d7fb7
root <g.irakiza@alustudent.com>
2024-03-03 10:04:18 +0000

    new

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -3,15 +3,23 @@
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """function that fetches number_of_subscribers"""
+    """
+    Function to get number of Subscribers
+    """
+    
     URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    HEADERS = {"User-Agent": "Chrome/122.0.6261.95"}
+    HEADERS = {"User-Agent": "I am Learning python"}
 
     try:
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        return RESPONSE.json().get("data").get("subscribers")
+        RESPONSE.raise_for_status()  # Raise an exception for bad status codes
 
-    except Exception:
+        data = RESPONSE.json()
+        subscribers = data.get("data", {}).get("subscribers")
+        return subscribers if subscribers is not None else 0
+
+    except requests.RequestException as e:
+        print("An error occurred: {}".format(e))
         return 0
+
