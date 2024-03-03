2dc058a34c492b97231f0a047eaa9b07f3a128eb
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2024-03-03 12:07:54 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,28 +1,8 @@
 #!/usr/bin/python3
 """Module for task 0"""
 
+
 def number_of_subscribers(subreddit):
-<<<<<<< HEAD
-    """
-    Function to get number of Subscribers
-    """
-    
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    HEADERS = {"User-Agent": "I am Learning python"}
-
-    try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        RESPONSE.raise_for_status()  # Raise an exception for bad status codes
-
-        data = RESPONSE.json()
-        subscribers = data.get("data", {}).get("subscribers")
-        return subscribers if subscribers is not None else 0
-
-    except requests.RequestException as e:
-        print("An error occurred: {}".format(e))
-        return 0
-
-=======
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
     import requests
@@ -35,4 +15,3 @@ def number_of_subscribers(subreddit):
         return 0
 
     return sub_info.json().get("data").get("subscribers")
->>>>>>> cefc989a1317e63e87278cf1ee250160dc9d75f2
