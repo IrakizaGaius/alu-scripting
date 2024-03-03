cefc989a1317e63e87278cf1ee250160dc9d75f2
IrakizaGaius <111004500+IrakizaGaius@users.noreply.github.com>
2024-03-03 12:03:02 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and
-returns the number of subscribers
-(not active users, total subscribers)
-for a given subreddit.
-If an invalid subreddit is given,
-the function should return 0.
-"""
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    base_url = 'https://www.reddit.com'
-    api_uri = '{base}/r/{subreddit}/about.json'.format(base=base_url,
-                                                       subreddit=subreddit)
-    user_agent = {'User-Agent': 'Python/requests'}
-    res = requests.get(api_uri, headers=user_agent, allow_redirects=False)
-    if res.status_code in [302, 404]:
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
-    print("OK")
-    return res.json().get('data').get('subscribers')
+
+    return sub_info.json().get("data").get("subscribers")
