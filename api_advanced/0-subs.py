c41b2e68f961bfd3372aad37f902b47bea3fa924
root <g.irakiza@alustudent.com>
2024-03-07 07:14:09 -0800

    Custom User header

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Windows 10 Pro:Chrome:v122.0.6261.112 (by /u/Gaius2324)"}
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36(by /u/Gaius2324)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
